c1da5e8a0497c6e29ad7745f7ee99affc6bbf55e
thaipokecard <thaipokecard@hotmail.com>
2022-12-08 19:53:39 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment #3 Hello User',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Assignment #3 Hello User'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String username = 'User';
+
+  void _loginUser() {
+    setState(() {
+      //TODO
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello $username',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _loginUser,
+        tooltip: 'login',
+        child: const Icon(Icons.login),
+      ),
+    );
+  }
+}
+
+/*
+Provided Async function getUserInfo
+ */
+Future<String?> getUserInfo() async {
+  final usernameList = ['Peter','David',null];
+  return Future.delayed(const Duration(seconds: 3),() => usernameList[Random().nextInt(2)]);
+}
